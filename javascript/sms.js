4904e87ad130e62954a73ad79a0816e85dce7129
yourlogarithm <vlad.sebastian.cretu.dev@gmail.com>
2022-12-13 17:51:02 +0200

    Fixed sms.js, main script extended to frida server install and apk install + debug

--- javascript/sms.js
+++ javascript/sms.js
@@ -1,67 +1,10 @@
-const SmsMangaer = Java.use('android.telephony.SmsManager');
+const SmsManager = Java.use('android.telephony.SmsManager');
 
 function logSmsSent(destination, body) {
-    console.log('SmsSent:\n')
-    console.log('\tdestination: ' + destination);
-    console.log('\tbody: ' + body);
+    console.log('SmsSent:')
+    console.log('  destination: ' + destination);
+    console.log('  body: ' + body);
 };
-
-// SmsManager.sendDataMessage.overload(
-//     'java.lang.String', 
-//     'java.lang.String', 
-//     'short', 
-//     'byte[]', 
-//     'android.app.PendingIntent', 
-//     'android.app.PendingIntent'
-// ).implementation = function (destinationAddress, scAddressm, destinationPort, data, sentIntent, deliveryIntent) {
-//     logSmsSent(destinationAddress, data);
-//     return this.sendDataMessage(destinationAddress, scAddressm, destinationPort, data, sentIntent, deliveryIntent);
-// };
-
-// SmsManager.sendMultimediaMessage.overload(
-//     'android.content.Context',
-//     'android.net.Uri',
-//     'java.lang.String',
-//     'android.os.Bundle',
-//     'android.app.PendingIntent'
-// ).implementation = function (context, contentUri, locationUrl, configOverrides, sentIntent) {
-//     logSmsSent(locationUrl, contentUri);
-//     return this.sendMultimediaMessage(context, contentUri, locationUrl, configOverrides, sentIntent);
-// };
-
-// SmsManager.sendMultipartTextMessage.overload(
-//     'java.lang.String',
-//     'java.lang.String',
-//     'java.util.List<java.lang.String>',
-//     'java.util.List<PendingIntent>',
-//     'java.util.List<PendingIntent>',
-//     'long'
-// ).implementation = function (destinationAddress, scAddress, parts, sentIntents, deliveryIntents, messageId) {
-//     logSmsSent(destinationAddress, parts);
-//     return sendMultipartTextMessage(destinationAddress, scAddress, parts, sentIntents, deliveryIntents, messageId);
-// };
-
-// SmsManager.sendMultipartTextMessage.overload(
-//     'java.lang.String',
-//     'java.lang.String',
-//     'java.util.List<java.lang.String>',
-//     'java.util.List<PendingIntent>',
-//     'java.util.List<PendingIntent>',
-// ).implementation = function (destinationAddress, scAddress, parts, sentIntents, deliveryIntents) {
-//     logSmsSent(destinationAddress, parts);
-//     return sendMultipartTextMessage(destinationAddress, scAddress, parts, sentIntents, deliveryIntents);
-// };
-
-// SmsManager.sendMultipartTextMessage.overload(
-//     'java.lang.String',
-//     'java.lang.String',
-//     'java.util.List<java.lang.String>',
-//     'java.lang.String',
-//     'java.lang.String',
-// ).implementation = function (destinationAddress, scAddress, parts, sentIntents, deliveryIntents, packageName, attributionTag) {
-//     logSmsSent(destinationAddress, parts);
-//     return sendMultipartTextMessage(destinationAddress, scAddress, parts, sentIntents, deliveryIntents, packageName, attributionTag);
-// };
 
 SmsManager.sendTextMessage.overload(
     'java.lang.String',
@@ -91,4 +34,64 @@ const BroadcastReceiver = Java.use('android.content.BroadcastReceiver');
 BroadcastReceiver.onReceive.overload('android.content.Context', 'android.content.Intent').implementation = function (context, intent) {
     console.log('Sms receieved by app');
     return this.onReceive(context, intent);
+}
+
+
+SmsManager.sendDataMessage.overload(
+    'java.lang.String', 
+    'java.lang.String', 
+    'short', 
+    '[B', 
+    'android.app.PendingIntent', 
+    'android.app.PendingIntent'
+).implementation = function (destinationAddress, scAddressm, destinationPort, data, sentIntent, deliveryIntent) {
+    logSmsSent(destinationAddress, data);
+    return this.sendDataMessage(destinationAddress, scAddressm, destinationPort, data, sentIntent, deliveryIntent);
+};
+
+SmsManager.sendMultimediaMessage.overload(
+    'android.content.Context',
+    'android.net.Uri',
+    'java.lang.String',
+    'android.os.Bundle',
+    'android.app.PendingIntent'
+).implementation = function (context, contentUri, locationUrl, configOverrides, sentIntent) {
+    logSmsSent(locationUrl, contentUri);
+    return this.sendMultimediaMessage(context, contentUri, locationUrl, configOverrides, sentIntent);
+};
+
+SmsManager.sendMultipartTextMessage.overload(
+    'java.lang.String', 
+    'java.lang.String', 
+    'java.util.ArrayList', 
+    'java.util.ArrayList', 
+    'java.util.ArrayList'
+).implementation = function (destinationAddress, scAddress, parts, sentIntents, deliveryIntents) {
+    logSmsSent(destinationAddress, parts);
+    return this.sendMultipartTextMessage(destinationAddress, scAddress, parts, sentIntents, deliveryIntents);
+};
+
+SmsManager.sendMultipartTextMessage.overload(
+    'java.lang.String', 
+    'java.lang.String', 
+    'java.util.List', 
+    'java.util.List', 
+    'java.util.List', 
+    'long'
+).implementation = function (destinationAddress, scAddress, parts, sentIntents, deliveryIntents, messageId) {
+    logSmsSent(destinationAddress, parts);
+    return this.sendMultipartTextMessage(destinationAddress, scAddress, parts, sentIntents, deliveryIntents, messageId);
+}
+
+SmsManager.sendMultipartTextMessage.overload(
+    'java.lang.String', 
+    'java.lang.String', 
+    'java.util.List', 
+    'java.util.List', 
+    'java.util.List', 
+    'java.lang.String', 
+    'java.lang.String'
+).implementation = function (destinationAddress, scAddress, parts, sentIntents, deliveryIntents, packageName, attributionTag) {
+    logSmsSent(destinationAddress, parts);
+    return this.sendMultipartTextMessage(destinationAddress, scAddress, parts, sentIntents, deliveryIntents, packageName, attributionTag);
 }
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import frida, sys, time, argparse, re, os
+import frida, sys, time, argparse, re, os, subprocess
 from utils import on_message
+from multiprocessing import Process
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 class keyvalue(argparse.Action):
     # Constructor calling
@@ -12,30 +15,73 @@ class keyvalue(argparse.Action):
             # assign into dictionary
             getattr(namespace, self.dest)[key] = value
 
-if __name__ == '__main__':
+def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('pkg', type=str, help='Package name / activity to hook')
-    parser.add_argument('js', help='JS file(s) to hook', nargs='+')
-    parser.add_argument('-e', '--extras', help='Extra arguments to pass to the hooker', nargs='*', action=keyvalue)
+    subparsers = parser.add_subparsers(help='Action', dest='action')
+
+    setup_parser = subparsers.add_parser('setup', help='Setup frida android server on device',)
+    setup_parser.add_argument('server', help='Frida server version')
+
+    hook_parser = subparsers.add_parser('hook', help='Hook a package')
+    hook_parser.add_argument('js', help='JS file(s) to hook', nargs='+')
+    hook_parser.add_argument('pkg', type=str, help='Package name / activity to hook')
+    hook_parser.add_argument('--exportjs', '-ex', help='Export JS to file', action='store_true')
+    hook_parser.add_argument('-e', '--extras', help='Extra arguments to pass to the hooker', nargs='*', action=keyvalue)
+
+    full_parser = subparsers.add_parser('full', help='Setup frida server, install apk hook methods all at once')
+    full_parser.add_argument('server', help='Frida server version')
+    full_parser.add_argument('js', help='JS file(s) to hook', nargs='+')
+    full_parser.add_argument('filename', help='Filename of apk to install and debug')
+    full_parser.add_argument('pkg', type=str, help='Package name / activity to hook')
+    full_parser.add_argument('--exportjs', '-ex', help='Export JS to file', action='store_true')
+    full_parser.add_argument('-e', '--extras', help='Extra arguments to pass to the hooker', nargs='*', action=keyvalue)
+
     args = parser.parse_args()
 
+    if not args.action:
+        parser.print_help()
+        sys.exit(1)
+
+    return args
+
+
+def setup(server):
+    server_path = os.path.join(SCRIPT_PATH, 'servers', server)
+    output = '/data/local/tmp/frida-server'
+    subprocess.call(f'adb push {server_path} {output}', shell=True)
+    subprocess.call(f'adb shell "su 0 chmod 755 {output}"', shell=True)
+    if os.fork() != 0:
+        return
+    subprocess.call(f'adb shell "su 0 {output}"', shell=True)
+
+
+def setup_wrapper(server):
+    p = Process(target=setup, args=(server,))
+    p.start()
+    p.join()
+    print('Server running')
+
+
+def hook(js_filenames, export_js, pkg, extras):
     jscode = 'Java.perform(function () {\n'
-    for js in args.js:
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'javascript', js), 'r') as f:
+    for js in js_filenames:
+        with open(os.path.join(SCRIPT_PATH, 'javascript', js), 'r') as f:
             code = f.read()
-            if args.extras:
-                for k, v in args.extras.items():
+            if extras:
+                for k, v in extras.items():
                     code = code.replace(f'__{k}__', v)
             code = re.sub(r'\'__([a-z0-9]*_)*_\'', '\'\'', code)
             jscode += code
     jscode += '});'
     
-    print(jscode)
+    if export_js:
+        with open('rpc.js', 'w') as f:
+            f.write(jscode)
 
     device = frida.get_usb_device()
 
     try:
-        pid = frida.get_usb_device().spawn([args.pkg])
+        pid = frida.get_usb_device().spawn([pkg])
         device.resume(pid)
         time.sleep(1)
         session = device.attach(pid)
@@ -46,3 +92,21 @@ if __name__ == '__main__':
         sys.stdin.read()
     except frida.NotSupportedError:
         raise
+
+
+def install(filename):
+    subprocess.call(f'adb install -r {filename}', shell=True)
+
+
+if __name__ == '__main__':
+    args = get_arguments()
+
+    if args.action == 'setup':
+        setup_wrapper(args.server)
+    elif args.action == 'hook':
+        hook(args.js, args.exportjs, args.pkg, args.extras)
+    else:
+        setup_wrapper(args.server)
+        time.sleep(1)
+        install(args.filename)
+        hook(args.js, args.exportjs, args.pkg, args.extras)
